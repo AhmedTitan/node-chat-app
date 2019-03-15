46e87fffa278fdfa3ac9f4e991119e6bd6d5b012
Ahmed <ahmedtitan@gmail.com>
2019-03-15 19:55:43 +0530

    log output changed

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ io.on('connection', (socket) => {
     });
     
     socket.on('createMessage', (message, callback) => {
-        console.log(message);
+        console.log('New message: ',message);
         var user = users.getUser(socket.id);
         if(user && isRealString(message.text)){
             io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
